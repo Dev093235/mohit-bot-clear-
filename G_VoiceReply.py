6c9558a474eb6095d4c1e86a755a87be07908ad8
Dev093235 <dev431140@gmail.com>
2025-03-08 21:52:46 +0530

    Update G_VoiceReply.py

--- G_VoiceReply.py
+++ G_VoiceReply.py
@@ -1,1 +1,11 @@
+# G_VoiceReply.py - Converts text replies into voice
+import pyttsx3
 
+def voice_reply(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    user_msg = input("Enter a message: ")
+    voice_reply(user_msg)
